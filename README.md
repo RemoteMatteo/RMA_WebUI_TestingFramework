2db1343787909a57c76efe650c44871ccb0b2e1f
RemoteMatteo <mateusz.opitz@remotemyapp.com>
2021-03-23 22:43:05 +0100

    updated readme

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ git clone https://github.com/RemoteMatteo/RMA_WebUI_TestingFramework.git<br>
 
 #2. Build module <br>
 mvn clean install / build module in IntelIJ<br>
+
+#3. Uncomment webdriver depending on OS <br>
+D:\Frameworks\RMA_WebUI_TestingFramework\src\test\java\Tools\BrowserActions.java::14-17
--- src/test/java/Tools/BrowserActions.java
+++ src/test/java/Tools/BrowserActions.java
@@ -10,7 +10,12 @@ public class BrowserActions {
     static WebDriver driver;
 
     public static WebDriver returnDriver() {
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+
+//TODO:        uncomment in case of running on WINDOWS
+///        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+//TODO:        uncomment in case of running on MacOS
+///        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+
         return Objects.requireNonNullElseGet(driver, ChromeDriver::new);
     }
 
